20a91c2d2d3aabfbfae3adcc295fae910f43c4dc
AdBergman <bergman.adrian@gmail.com>
2019-02-06 17:36:34 +0100

    Finished FlightRepo tests.

--- src/test/java/com/bergman/traveldemo/repository/FlightRepoTest.java
+++ src/test/java/com/bergman/traveldemo/repository/FlightRepoTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.orm.jpa.JpaObjectRetrievalFailureException;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.bergman.traveldemo.domain.Flight;
@@ -32,5 +33,53 @@ public class FlightRepoTest {
         Flight flight = repository.save(new Flight().builder().origin("STO").destination("LON").build());
         assertThat(flight).hasFieldOrPropertyWithValue("origin", "STO");
         assertThat(flight).hasFieldOrPropertyWithValue("destination", "LON");
+    }
+
+    @Test
+    public void shouldFindFlightById(){
+        Flight flight1 = new Flight().builder().origin("STO").destination("LON").build();
+        entityManager.persist(flight1);
+
+        Flight flight2 = new Flight().builder().origin("GOT").destination("LON").build();
+        entityManager.persist(flight2);
+
+        Flight testFlight = repository.getOne(flight1.getId());
+        assertThat(testFlight).isEqualTo(flight1);
+    }
+
+    @Test
+    public void shouldFindAllFlights() {
+        Flight flight1 = new Flight().builder().origin("STO").destination("LON").build();
+        entityManager.persist(flight1);
+
+        Flight flight2 = new Flight().builder().origin("GOT").destination("LON").build();
+        entityManager.persist(flight2);
+
+        Flight flight3 = new Flight().builder().origin("NYC").destination("LON").build();
+        entityManager.persist(flight3);
+
+        List<Flight> flightList = repository.findAll();
+
+        assertThat(flightList).hasSize(3).contains(flight1, flight2, flight3);
+    }
+
+    @Test(expected = JpaObjectRetrievalFailureException.class)
+    public void shouldDeleteFlightById(){
+        Flight flight1 = new Flight().builder().origin("STO").destination("LON").build();
+        entityManager.persist(flight1);
+
+        Flight flight2 = new Flight().builder().origin("GOT").destination("LON").build();
+        entityManager.persist(flight2);
+
+        repository.deleteById(flight1.getId());
+        assertThat(flight1).isNotEqualTo(repository.getOne(flight1.getId()));
+    }
+
+    @Test
+    public void shouldDeleteAllFlights() {
+        entityManager.persist(new Flight().builder().origin("STO").destination("LON").build());
+        entityManager.persist(new Flight().builder().origin("LON").destination("STO").build());
+        repository.deleteAll();
+        assertThat(repository.findAll()).isEmpty();
     }
 }
